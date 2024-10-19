8317478e46f0b60d85e2b0f900db6df3b84e5602
YeonJun7 <yeonjunapex.gmail.com>
2024-10-19 10:58:13 +0900

    환경설정 파일 세팅 할때 환경변수 기본값과 환경 변수 값 조건에 따라 사용 하는 법
    spring:
      application:
        name: ${SPRING_APP_NAME:springsection1}
    만약 SPRING_APP_NAME 환경변수가 있으면 그 값이 들어가고 없으면 springsection1이 들어간다.

---
+++ section1/src/main/java/com/security/section1/EazyBankBackendApplication.java
@@ -0,0 +1,15 @@
+package com.security.section1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazyBankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazyBankBackendApplication.class, args);
+	}
+
+}
+
+
